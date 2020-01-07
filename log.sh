e00535c0c0f3bf3c6a3a73a0c4909f0bf78a19af
Nicolaas <github@sunnysideup.co.nz>
2020-01-07 13:31:01 +1300

    Update log.sh

--- log.sh
+++ log.sh
@@ -1,1 +1,4 @@
 git log --pretty="%C(Yellow)%h  | %C(reset)%ad | %C(Cyan)%an: | %C(reset)%s" --all --date=short
+
+# for specific dates
+git log --after="2016-01-31" --until="2017-03-10" --pretty="%C(Yellow)%h  | %C(reset)%ad | %C(Cyan)%an: | %C(reset)%s" --all --date=short
